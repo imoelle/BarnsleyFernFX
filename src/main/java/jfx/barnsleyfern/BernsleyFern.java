779221cf58375bbf8bf940fd0cbac40888893290
IngoHome <moeller.ingo@gmx.de>
2021-02-18 19:08:41 +0100

    create jfx project

---
+++ src/main/java/jfx/barnsleyfern/BernsleyFern.java
@@ -0,0 +1,16 @@
+package jfx.barnsleyfern;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BernsleyFern extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
